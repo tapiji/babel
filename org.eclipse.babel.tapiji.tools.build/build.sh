40ac64a981cd719c07a30eb2bbb0c67a4e82a1a0
Martin Reiterer <martin.reiterer@inso.tuwien.ac.at>
2012-11-27 14:50:31 +0100

    Removes outdated PDE build project
    The PDE build based buildscript project is outdated now. It has already been replaced by a tycho based build approach. This change removes the outdated project files.

--- org.eclipse.babel.tapiji.tools.build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ECLIPSE_BASE=~/dev/tapiji/build/eclipse
-
-java -jar $ECLIPSE_BASE/plugins/org.eclipse.equinox.launcher_1.2.0.v20110502.jar \
-     -application org.eclipse.ant.core.antRunner \
-     -buildfile build.xml \
-     -DbaseLocation=$ECLIPSE_BASE
