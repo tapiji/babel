42892352a98379e685cd522dc5e666d16b36c0e0
Martin Reiterer <martin.reiterer@inso.tuwien.ac.at>
2013-03-10 13:45:43 +0100

    Fixes invalid plug-in id in rbmanager.ui plug-in

--- org.eclipse.babel.tapiji.tools.rbmanager.ui/src/org/eclipse/babel/tapiji/tools/rbmanager/RBManagerActivator.java
+++ org.eclipse.babel.tapiji.tools.rbmanager.ui/src/org/eclipse/babel/tapiji/tools/rbmanager/RBManagerActivator.java
@@ -19,7 +19,7 @@ import org.osgi.framework.BundleContext;
  */
 public class RBManagerActivator extends AbstractUIPlugin {
     // The plug-in ID
-    public static final String PLUGIN_ID = "org.eclipse.babel.tapiji.tools.rbmanager"; //$NON-NLS-1$
+    public static final String PLUGIN_ID = "org.eclipse.babel.tapiji.tools.rbmanager.ui"; //$NON-NLS-1$
     // The shared instance
     private static RBManagerActivator plugin;
 
