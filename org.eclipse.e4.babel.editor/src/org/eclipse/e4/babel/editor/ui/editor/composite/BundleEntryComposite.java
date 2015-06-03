27bd6a836dc27b0e27732e0e52f0fce7cfe9be9a
Christian Behon <christian.behon@gmail.com>
2015-06-03 09:07:04 +0200

    Add key listener

--- org.eclipse.e4.babel.editor/src/org/eclipse/e4/babel/editor/ui/editor/composite/BundleEntryComposite.java
+++ org.eclipse.e4.babel.editor/src/org/eclipse/e4/babel/editor/ui/editor/composite/BundleEntryComposite.java
@@ -1,17 +1,24 @@
 package org.eclipse.e4.babel.editor.ui.editor.composite;
 
 
+import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.TextViewer;
+import org.eclipse.jface.text.TextViewerUndoManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipselabs.e4.tapiji.logger.Log;
 
 
 public final class BundleEntryComposite extends ScrolledComposite {
+
+    private static final int UNDO_LEVEL = 20;
 
     public static BundleEntryComposite create(final Composite sashForm) {
         return new BundleEntryComposite(sashForm, SWT.V_SCROLL | SWT.H_SCROLL);
@@ -41,10 +48,51 @@ public final class BundleEntryComposite extends ScrolledComposite {
             lblNewLabel_1.setBounds(0, 0, 62, 15);
             lblNewLabel_1.setText("New Label" + i);
 
-            final TextViewer textViewer = new TextViewer(comp, SWT.BORDER);
+            final TextViewer textViewer = new TextViewer(comp, SWT.V_SCROLL | SWT.WRAP | SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
+            textViewer.setDocument(new Document());
+
+
+            TextViewerUndoManager undoManager = new TextViewerUndoManager(UNDO_LEVEL);
+            textViewer.setUndoManager(undoManager);
+
+
             final StyledText styledText = textViewer.getTextWidget();
 
-            styledText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+            styledText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 4, 4));
+
+
+            styledText.addKeyListener(new KeyAdapter() {
+
+                @Override
+                public void keyReleased(KeyEvent event) {
+                    Log.d("sdsd", "sdsdsd");
+                    if (isKeyCombination(event, SWT.CTRL, 'z')) {
+                        undoManager.undo();
+                    } else if (isKeyCombination(event, SWT.CTRL, 'y')) {
+                        undoManager.redo();
+                    } else if (isKeyCombination(event, SWT.CTRL, 'a')) {
+                        textViewer.setSelectedRange(0, textViewer.getDocument().getLength());
+                    } else {
+                        /*
+                         * StyledText eventBox = (StyledText) event.widget;
+                         * final ITextEditor editor = resourceManager.getSourceEditor(locale).getEditor();
+                         * // Text field has changed: make editor dirty if not already
+                         * if (textBeforeUpdate != null && !textBeforeUpdate.equals(eventBox.getText())) {
+                         * // Make the editor dirty if not already. If it is,
+                         * // we wait until field focus lost (or save) to
+                         * // update it completely.
+                         * if (!editor.isDirty()) {
+                         * int caretPosition = eventBox.getCaretOffset();
+                         * updateBundleOnChanges();
+                         * eventBox.setSelection(caretPosition);
+                         * }
+                         * //utoDetectRequiredFont(eventBox.getText());
+                         */
+
+                    }
+                }
+            });
+
         }
         this.setContent(comp);
         this.setMinSize(comp.computeSize(SWT.DEFAULT, SWT.DEFAULT));
@@ -52,5 +100,9 @@ public final class BundleEntryComposite extends ScrolledComposite {
         this.setExpandVertical(true);
         this.setShowFocusedControl(true);
 
+    }
+
+    private static boolean isKeyCombination(KeyEvent event, int modifier1, int keyCode) {
+        return (event.keyCode == keyCode && event.stateMask == modifier1);
     }
 }
