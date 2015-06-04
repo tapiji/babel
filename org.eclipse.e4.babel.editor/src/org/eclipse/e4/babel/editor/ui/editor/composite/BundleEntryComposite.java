bd7eb000b04eaf0b4e00a674eb8961958775d18f
Christian Behon <christian.behon@gmail.com>
2015-06-04 09:36:05 +0200

    Add text listener

--- org.eclipse.e4.babel.editor/src/org/eclipse/e4/babel/editor/ui/editor/composite/BundleEntryComposite.java
+++ org.eclipse.e4.babel.editor/src/org/eclipse/e4/babel/editor/ui/editor/composite/BundleEntryComposite.java
@@ -5,51 +5,90 @@ import org.eclipse.jface.text.TextViewer;
 import org.eclipse.jface.text.TextViewerUndoManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.events.TraverseEvent;
 import org.eclipse.swt.events.TraverseListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
 import org.eclipselabs.e4.tapiji.logger.Log;
 
 
-final class BundleEntryComposite extends Composite implements KeyListener, TraverseListener {
+final class BundleEntryComposite extends Composite implements KeyListener, TraverseListener, SelectionListener, FocusListener {
 
     private static final int UNDO_LEVEL = 20;
     private static final String TAG = BundleEntryComposite.class.getSimpleName();
     private final TextViewerUndoManager undoManager = new TextViewerUndoManager(UNDO_LEVEL);
     private TextViewer textViewer;
-    private I18nComposite i18nComposite;
+    private IBundleEntryComposite listener;
 
-    private BundleEntryComposite(final Composite parent, final int style, I18nComposite i18nComposite) {
+    public interface IBundleEntryComposite {
+
+        void onLocaleClick();
+
+        void setNextFocusDown();
+
+        void setNextFocusUp();
+
+        void onFocusChange(BundleEntryComposite bundleEntryComposite);
+
+    }
+
+    private BundleEntryComposite(final Composite parent, final int style) {
         super(parent, style);
 
         setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
-        this.i18nComposite = i18nComposite;
-
-
-        setLayout(new GridLayout(2, false));
+        GridLayout gridLayout = new GridLayout(3, false);
+        gridLayout.marginWidth = 0;
+        setLayout(gridLayout);
 
         Composite composite = new Composite(this, SWT.NONE);
-        composite.setLayout(new GridLayout(2, false));
+        GridLayout gl_composite = new GridLayout(3, false);
+        gl_composite.verticalSpacing = 0;
+        gl_composite.marginWidth = 0;
+        gl_composite.marginHeight = 0;
+        composite.setLayout(gl_composite);
 
         Label lblNewLabel = new Label(composite, SWT.NONE);
         lblNewLabel.setText("New Label");
 
+
+        Link localeName = new Link(composite, SWT.NONE);
+        GridData localeNameLayoutData = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+        localeNameLayoutData.horizontalIndent = 5;
+        localeName.setLayoutData(localeNameLayoutData);
+        localeName.setText("<a>Albanien</a>");
+        localeName.addSelectionListener(this);
+
+
         Label lblNewLabel_2 = new Label(composite, SWT.NONE);
+        GridData gd_lblNewLabel_2 = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+        gd_lblNewLabel_2.horizontalIndent = 5;
+        lblNewLabel_2.setLayoutData(gd_lblNewLabel_2);
         lblNewLabel_2.setText("New Label");
+        new Label(this, SWT.NONE);
 
         Composite composite_1 = new Composite(this, SWT.NONE);
-        composite_1.setLayout(new GridLayout(2, false));
+        GridLayout gl_composite_1 = new GridLayout(2, false);
+        gl_composite_1.marginHeight = 0;
+        gl_composite_1.marginWidth = 0;
+        composite_1.setLayout(gl_composite_1);
         composite_1.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 
         Label lblNewLabel_3 = new Label(composite_1, SWT.NONE);
         lblNewLabel_3.setText("New Label");
 
         Label lblNewLabel_1 = new Label(composite_1, SWT.NONE);
+        GridData gd_lblNewLabel_1 = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+        gd_lblNewLabel_1.horizontalIndent = 5;
+        lblNewLabel_1.setLayoutData(gd_lblNewLabel_1);
         lblNewLabel_1.setText("New Label");
 
         Composite composite_2 = new Composite(this, SWT.NONE);
@@ -58,13 +97,16 @@ final class BundleEntryComposite extends Composite implements KeyListener, Trave
         gl_composite_2.marginWidth = 0;
         gl_composite_2.verticalSpacing = 0;
         composite_2.setLayout(gl_composite_2);
-        composite_2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 2));
+        composite_2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 3, 2));
 
-        TextViewer textViewer = new TextViewer(composite_2, SWT.MULTI | SWT.WRAP | SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
+        TextViewer textViewer = new TextViewer(composite_2, SWT.BORDER);
         StyledText styledText = textViewer.getTextWidget();
         GridData gd_styledText = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
         gd_styledText.minimumHeight = 40;
         styledText.setLayoutData(gd_styledText);
+        styledText.addFocusListener(this);
+        styledText.addTraverseListener(this);
+        styledText.addKeyListener(this);
     }
 
     private static boolean isKeyCombination(final KeyEvent event, final int mask, final int keyCode) {
@@ -77,7 +119,7 @@ final class BundleEntryComposite extends Composite implements KeyListener, Trave
 
     @Override
     public void keyReleased(final KeyEvent event) {
-        Log.d(TAG, "Event: " + event);
+        Log.d(TAG, "keyReleased: " + event);
         if (isKeyCombination(event, SWT.CTRL, 'z')) {
             undoManager.undo();
         } else if (isKeyCombination(event, SWT.CTRL, 'y')) {
@@ -91,19 +133,57 @@ final class BundleEntryComposite extends Composite implements KeyListener, Trave
 
     @Override
     public void keyTraversed(TraverseEvent event) {
-        if (event.character == SWT.TAB) {
-            event.doit = true;
-            event.detail = SWT.TRAVERSE_NONE;
-        } else if (event.keyCode == SWT.ARROW_DOWN && event.stateMask == SWT.CTRL) {
-            event.doit = true;
-            event.detail = SWT.TRAVERSE_NONE;
-        } else if (event.keyCode == SWT.ARROW_UP && event.stateMask == SWT.CTRL) {
-            event.doit = true;
-            event.detail = SWT.TRAVERSE_NONE;
+        Log.d(TAG, "keyTraversed: " + event.toString());
+        if (listener != null) {
+            if (event.character == SWT.TAB) {
+                event.doit = true;
+                event.detail = SWT.TRAVERSE_NONE;
+                if (event.stateMask == 0) {
+                    listener.setNextFocusDown();
+                } else if (event.stateMask == SWT.SHIFT) {
+                    listener.setNextFocusUp();
+                }
+            } else if (event.keyCode == SWT.ARROW_DOWN && event.stateMask == SWT.CTRL) {
+                event.doit = true;
+                event.detail = SWT.TRAVERSE_NONE;
+            } else if (event.keyCode == SWT.ARROW_UP && event.stateMask == SWT.CTRL) {
+                event.doit = true;
+                event.detail = SWT.TRAVERSE_NONE;
+            }
         }
     }
 
-    public static BundleEntryComposite create(final Composite parent, I18nComposite i18nComposite) {
-        return new BundleEntryComposite(parent, SWT.BORDER, i18nComposite);
+    public static BundleEntryComposite create(final Composite parent) {
+        return new BundleEntryComposite(parent, SWT.BORDER);
+    }
+
+    @Override
+    public void widgetSelected(SelectionEvent event) {
+        Log.d(TAG, "widgetSelected: " + event.toString());
+        if (null != listener) {
+            listener.onLocaleClick();
+        }
+    }
+
+    @Override
+    public void widgetDefaultSelected(SelectionEvent e) {
+    }
+
+    public void addListener(IBundleEntryComposite entryListener) {
+        if (null != entryListener) {
+            listener = entryListener;
+        }
+    }
+
+    @Override
+    public void focusGained(FocusEvent event) {
+        Log.d(TAG, "focusGained: " + event.toString());
+        if (null != listener) {
+            listener.onFocusChange(this);
+        }
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
     }
 }
--- org.eclipse.e4.babel.editor/src/org/eclipse/e4/babel/editor/ui/editor/composite/I18nComposite.java
+++ org.eclipse.e4.babel.editor/src/org/eclipse/e4/babel/editor/ui/editor/composite/I18nComposite.java
@@ -7,11 +7,14 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipselabs.e4.tapiji.logger.Log;
 
 
-public final class I18nComposite extends ScrolledComposite {
+public final class I18nComposite extends ScrolledComposite implements BundleEntryComposite.IBundleEntryComposite {
 
+    private static final String TAG = I18nComposite.class.getSimpleName();
     private final List<BundleEntryComposite> bundleEntryComposite = new ArrayList<>();
+    private BundleEntryComposite activeBundleEntryComposite;
 
     public static I18nComposite create(final Composite sashForm) {
         return new I18nComposite(sashForm, SWT.V_SCROLL | SWT.H_SCROLL);
@@ -25,13 +28,39 @@ public final class I18nComposite extends ScrolledComposite {
         comp.setLayout(new GridLayout(1, false));
 
 
-        for (int i = 0; i < 30; i++) {
-            bundleEntryComposite.add(BundleEntryComposite.create(comp, this));
+        for (int i = 0; i < 2; i++) {
+            final BundleEntryComposite entry = BundleEntryComposite.create(comp);
+            entry.addListener(this);
+            bundleEntryComposite.add(BundleEntryComposite.create(comp));
         }
         this.setContent(comp);
         this.setMinSize(comp.computeSize(SWT.DEFAULT, SWT.DEFAULT));
         this.setExpandHorizontal(true);
         this.setExpandVertical(true);
         this.setShowFocusedControl(true);
+    }
+
+    @Override
+    public void onLocaleClick() {
+
+    }
+
+    @Override
+    public void setNextFocusDown() {
+        Log.d(TAG, "NextFocusDown");
+        if (null != activeBundleEntryComposite) {
+            final int index = bundleEntryComposite.indexOf(activeBundleEntryComposite);
+            Log.d(TAG, "INDEX: " + index);
+        }
+    }
+
+    @Override
+    public void setNextFocusUp() {
+
+    }
+
+    @Override
+    public void onFocusChange(BundleEntryComposite bundleEntryComposite) {
+        activeBundleEntryComposite = bundleEntryComposite;
     }
 }
