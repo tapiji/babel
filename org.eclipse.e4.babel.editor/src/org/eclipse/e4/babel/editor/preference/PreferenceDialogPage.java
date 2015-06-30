7f0723c28d4f768c7ad944ae9fb6186c6e316bcf
Christian Behon <christian.behon@gmail.com>
2015-07-01 00:19:54 +0200

    Add prefrence page

--- org.eclipse.e4.babel.editor/src/org/eclipse/e4/babel/editor/preference/PreferenceDialogPage.java
+++ org.eclipse.e4.babel.editor/src/org/eclipse/e4/babel/editor/preference/PreferenceDialogPage.java
@@ -1,57 +1,36 @@
 package org.eclipse.e4.babel.editor.preference;
 
 
-import org.eclipse.jface.preference.ColorFieldEditor;
-import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.util.PropertyChangeEvent;
-import org.eclipselabs.e4.tapiji.logger.Log;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 
 
-public class PreferenceDialogPage extends FieldEditorPreferencePage {
+public class PreferenceDialogPage extends PreferencePage {
 
 
     private static final String TAG = PreferenceDialogPage.class.getSimpleName();
 
-    public PreferenceDialogPage() {
-        super("sadd", FLAT);
-    }
 
     @Override
-    protected void createFieldEditors() {
-        //  BooleanFieldEditor bfe = new BooleanFieldEditor("myBoolean", "Boolean", getFieldEditorParent());
-        //7 addField(bfe);
+    protected Control createContents(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NONE);
+        GridLayout gridLayout = new GridLayout(2, false);
+
+        gridLayout.marginHeight = 0;
+        gridLayout.verticalSpacing = 0;
+        composite.setLayout(gridLayout);
 
 
-        // Add a color field
-        ColorFieldEditor cfe = new ColorFieldEditor("myColor", "Color:", getFieldEditorParent());
+        Button showGeneratedBy = new Button(composite, SWT.CHECK);
+        new Label(composite, SWT.NONE).setText("sdfdfsaf");
 
-        addField(cfe);
-
-        // Add a directory field
-        //  DirectoryFieldEditor dfe = new DirectoryFieldEditor("myDirectory", "Directory:", getFieldEditorParent());
-        // addField(dfe);
-
-        // Add a file field
-        //FileFieldEditor ffe = new FileFieldEditor("myFile", "File:", getFieldEditorParent());
-        // addField(ffe);
-
-        // Add a font field
-        // FontFieldEditor fontFe = new FontFieldEditor("myFont", "Font:", getFieldEditorParent());
-        //addField(fontFe);
-
-        // Add a radio group field
-        // RadioGroupFieldEditor rfe = new RadioGroupFieldEditor("myRadioGroup", "Radio Group", 2, new String[][] { {"First Value", "first"}, {"Second Value", "second"},
-        //         {"Third Value", "third"}, {"Fourth Value", "fourth"}}, getFieldEditorParent(), true);
-        // addField(rfe);
-
-        // Add a path field
-        //PathEditor pe = new PathEditor("myPath", "Path:", "Choose a Path", getFieldEditorParent());
-        //addField(pe);
+        return composite;
     }
 
-    @Override
-    public void propertyChange(PropertyChangeEvent event) {
-        super.propertyChange(event);
-        Log.d(TAG, "PROPERYT CHANGED: " + event);
-    }
+
 }
