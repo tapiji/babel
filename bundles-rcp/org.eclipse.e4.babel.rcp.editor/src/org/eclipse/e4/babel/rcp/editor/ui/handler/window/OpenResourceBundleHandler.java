73705b0d75855a2064d9d4cda2f77fea091299c9
Christian Behon <christian.behon@knusperleicht.at>
2017-02-14 20:18:48 +0100

    Change open resource bundle logic

--- bundles-rcp/org.eclipse.e4.babel.rcp.editor/src/org/eclipse/e4/babel/rcp/editor/ui/handler/window/OpenResourceBundleHandler.java
+++ bundles-rcp/org.eclipse.e4.babel.rcp.editor/src/org/eclipse/e4/babel/rcp/editor/ui/handler/window/OpenResourceBundleHandler.java
@@ -1,15 +1,23 @@
 package org.eclipse.e4.babel.rcp.editor.ui.handler.window;
 
+
 import org.eclipse.e4.babel.core.utils.BabelUtils;
 import org.eclipse.e4.babel.editor.ui.handler.window.AOpenResourceBundleHandler;
+import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 
+
 public class OpenResourceBundleHandler extends AOpenResourceBundleHandler {
 
-	@Override
-	protected String[] recentlyOpenedFiles(Shell shell) {
-		return BabelUtils.queryFileName(shell, "Open Resource-Bundle", SWT.OPEN, BabelUtils.PROPERTY_FILE_ENDINGS);
-	}
+    @Override
+    protected String[] recentlyOpenedFiles(Shell shell) {
+        return BabelUtils.queryFileName(shell, "Open Resource-Bundle", SWT.OPEN, BabelUtils.PROPERTY_FILE_ENDINGS);
+    }
 
+    @Override
+    @Execute
+    public void execute() {
+        super.execute();
+    }
 }
